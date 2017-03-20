4c795edcfe1abfd74c3ea28b5b0cb49d8b26fbcb
Eben Krapfl <eben@krapfl.org>
2017-03-20 06:31:19 -0500

    Started recipe-list and item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { RecipeBookComponent } from './recipe-book.component';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeBookComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
